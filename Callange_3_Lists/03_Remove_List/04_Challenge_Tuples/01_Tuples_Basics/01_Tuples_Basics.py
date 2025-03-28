f9d7e297aede9ed2423ccfd180d68a22b6c8b84c
Nargisnayab <1038392@pdsb.net>
2025-03-28 12:30:37 -0400

    Of coures

--- Callange_3_Lists/03_Remove_List/04_Challenge_Tuples/01_Tuples_Basics/01_Tuples_Basics.py
+++ Callange_3_Lists/03_Remove_List/04_Challenge_Tuples/01_Tuples_Basics/01_Tuples_Basics.py
@@ -9,7 +9,9 @@
 my_tuple = (42, 3.14, 'Python', True, (1, 2, 3))
 # Printing the entire tuple
 print("Tuple:" , my_tuple)
+
 # Accessing the third element
 print("The third element:" ,my_tuple[2])
+
 # Extracting and printing the first element of the nested tuple
 print("The first element of the nested tuple: " , my_tuple [4][0] )
---
+++ Callange_3_Lists/03_Remove_List/04_Challenge_Tuples/01_Tuples_Basics/02_Swap_Values/02_Swap_Values.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        Swap Values Using Tuples
+# Purpose:     Swaps two user - provided values using tuple unpacking
+# Author:      Mrs. NN
+# Created:     25-March-2025
+# Updated:     25-March-2025
+#------------------------------------------------------------------------
+# Taking input from the user
+num1 = int(input( "Enter the first number: "))
+num2 = int(input( " Enter the second number: "))
+# Storing values in a  tuple
+number = (num1, num2)
